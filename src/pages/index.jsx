ea2b397229b1c43500396be7a95561155d0ab3aa
stitchinteractive <boan@stitchinteractive.com>
2022-06-02 19:37:47 +0800

    home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -19,10 +19,10 @@ export const query = graphql`
     }
   }
 `
-function Hero (props) {
+function Hero(props) {
   return (
     <div className={container}>
-      <h1 className={intro}>Welcome to the GatsbyJS + Shopify Demo Store.</h1>
+      <h1 className={intro}>Welcome to the GatsbyJS + Shopify Demo Store!!!</h1>
       {!!process.env.GATSBY_DEMO_STORE && (
         <>
           <p className={callOut}>
