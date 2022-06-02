78fbc39aa50ac7ab346e91674bfe61a34e4fea8b
stitchinteractive <boan@stitchinteractive.com>
2022-06-02 20:05:14 +0800

    icons

--- src/icons/search.jsx
+++ src/icons/search.jsx
@@ -3,20 +3,16 @@ import * as React from "react"
 function SearchIcon(props) {
   return (
     <svg
-      width={24}
-      height={24}
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      {...props}
     >
-      <title>{"Search"}</title>
-      <circle cx={11} cy={11} r={7} stroke="currentColor" strokeWidth={2} />
       <path
-        stroke="currentColor"
-        strokeWidth={2}
-        strokeLinecap="round"
-        d="M15.914 16l4.596 4.596"
+        d="M10 18C11.775 17.9996 13.4988 17.4054 14.897 16.312L19.293 20.708L20.707 19.294L16.311 14.898C17.405 13.4997 17.9996 11.7754 18 10C18 5.589 14.411 2 10 2C5.589 2 2 5.589 2 10C2 14.411 5.589 18 10 18ZM10 4C13.309 4 16 6.691 16 10C16 13.309 13.309 16 10 16C6.691 16 4 13.309 4 10C4 6.691 6.691 4 10 4Z"
+        fill="#909090"
+        fill-opacity="0.8"
       />
     </svg>
   )
