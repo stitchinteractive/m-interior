d4144e9765b4c73266e1cfabefbe83363a697498
stitchinteractive <boan@stitchinteractive.com>
2022-06-04 13:43:27 +0800

    home with images

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -229,7 +229,7 @@ const HomePage = () => {
           <div className="row row_padding">
             <div className="col-md-6">
               <video width="100%" height="100%" controls>
-                <source src="/home/intro_connectors.mp4" type="video/mp4" />
+                <source src="/home/intro_colours.mp4" type="video/mp4" />
               </video>
             </div>
             <div className="col-md-6">
