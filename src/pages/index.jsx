3a26854a402abf1b6f3bf832ec99e2b97c1eb233
stitchinteractive <boan@stitchinteractive.com>
2022-06-05 10:31:28 +0800

    membership

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -314,26 +314,28 @@ const HomePage = () => {
       </div>
       <div className="bg_membership">
         <div className="container">
-          <div className="row row_padding align-items-end">
-            <div className="col-md-5">
-              <p>
-                <img src="/bg_membership.png" />
-              </p>
+          <div className="row d-flex">
+            <div className="col-md-5 align-self-end">
+              <div className="pt-5">
+                <img src="/membership.png" />
+              </div>
             </div>
-            <div className="col-md-7">
-              <h2 className="text-uppercase">Get exclusive with us</h2>
-              <p className="pb-5">
-                Join M.INT Club and be a part of something bigger! Gain access
-                to member-only benefits and stay informed with the latest
-                interior trends and news from us.
-              </p>
-              <p>
-                <Link to="/">
-                  <button type="button" className="btn btn-outline">
-                    Join now and enjoy 10% off your first purchase
-                  </button>
-                </Link>
-              </p>
+            <div className="col-md-7 align-self-center">
+              <div className="membership_padding">
+                <h2 className="text-uppercase">Get exclusive with us</h2>
+                <p className="pb-5">
+                  Join M.INT Club and be a part of something bigger! Gain access
+                  to member-only benefits and stay informed with the latest
+                  interior trends and news from us.
+                </p>
+                <p>
+                  <Link to="/">
+                    <button type="button" className="btn btn-outline">
+                      Join now and enjoy 10% off your first purchase
+                    </button>
+                  </Link>
+                </p>
+              </div>
             </div>
           </div>
         </div>
