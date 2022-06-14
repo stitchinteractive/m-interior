fb24ffabb3cf4a40cac54c349f2b81f1d1c7266f
stitchinteractive <boan@stitchinteractive.com>
2022-06-14 16:17:28 +0800

    home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -25,157 +25,29 @@ const HomePage = () => {
   return (
     <Layout>
       <div className="bg_black font_white">
-        {/* banner for mobile */}
-        <div className="d-md-none">
-          <Swiper
-            // install Swiper modules
-            modules={[Pagination, A11y]}
-            spaceBetween={50}
-            slidesPerView={1}
-            pagination={{ clickable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log("slide change")}
+        {/* banner */}
+        <Swiper
+          // install Swiper modules
+          modules={[Pagination, A11y]}
+          spaceBetween={50}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          <SwiperSlide
+            className={`${indexModule.banner_home} d-flex flex-column justify-content-center align-items-start h-100`}
+            style={{
+              background: "url(/home/banner_1.jpg) center center no-repeat",
+              backgroundSize: "cover",
+            }}
           >
-            <SwiperSlide
-              className={`${indexModule.banner_home} d-flex flex-column justify-content-center align-items-start h-100`}
-            >
-              <div className="text-center h-100">
-                <div>
-                  <img src="/home/banner_1.jpg" alt="Acacia Blocks" />
-                </div>
-                <div className={indexModule.padding_banner}>
-                  <h1 className="text-uppercase pb-3">Acacia Blocks</h1>
-                  <p className="pb-5">
-                    Find fun and freedom in transforming your spaces with our
-                    modular furniture series. Simply connect, stack and mount
-                    the blocks to build your own creative storage display.
-                  </p>
-                  <p>
-                    <Link to="/">
-                      <button
-                        type="button"
-                        className="btn btn-outline btn-outline-large"
-                      >
-                        Discover More
-                      </button>
-                    </Link>
-                  </p>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide
-              className={`${indexModule.banner_home} d-flex flex-column justify-content-center align-items-start h-100`}
-            >
-              <div className="text-center h-100">
-                <div>
-                  <img src="/home/banner_2.jpg" alt="M.INT club" />
-                </div>
-                <div className={indexModule.padding_banner}>
-                  <h1 className="text-uppercase pb-3">M.INT club</h1>
-                  <p>
-                    <div className="text-uppercase pb-3">
-                      <strong>is</strong> <i>more than just </i>
-                      <strong>a rewards program</strong>
-                    </div>
-                  </p>
-                  <p className="pb-5">
-                    Join our community and be a part of something bigger.
-                  </p>
-                  <p>
-                    <Link to="/">
-                      <button
-                        type="button"
-                        className="btn btn-outline btn-outline-large"
-                      >
-                        Join now and enjoy 10% off your first purchase
-                      </button>
-                    </Link>
-                  </p>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide
-              className={`${indexModule.banner_home} d-flex flex-column justify-content-center align-items-start h-100`}
-            >
-              <div className="text-center h-100">
-                <div>
-                  <img src="/home/banner_3.jpg" alt="Free delivery" />
-                </div>
-                <div className={indexModule.padding_banner}>
-                  <h1 className="text-uppercase">Free delivery to your door</h1>
-                  <p className="pb-5">
-                    Enjoy free delivery when you spend over $150 in a single
-                    purchase.
-                  </p>
-                  <p>
-                    <Link to="/">
-                      <button
-                        type="button"
-                        className="btn btn-outline btn-outline-large"
-                      >
-                        Shop Now
-                      </button>
-                    </Link>
-                  </p>
-                </div>
-              </div>
-            </SwiperSlide>
-          </Swiper>
-        </div>
-        {/* banner for mobile */}
-        {/* banner for tablets and larger screens */}
-        <div className="d-none d-md-block">
-          <Swiper
-            // install Swiper modules
-            modules={[Pagination, A11y]}
-            spaceBetween={50}
-            slidesPerView={1}
-            pagination={{ clickable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log("slide change")}
-          >
-            <SwiperSlide
-              className={`${indexModule.banner_home} d-flex flex-column justify-content-center align-items-start h-100`}
-              style={{
-                background: "url(/home/banner_1.jpg) center center no-repeat",
-                backgroundSize: "cover",
-              }}
-            >
-              <div className="d-flex flex-column justify-content-center align-items-start h-100">
-                <h1 className="text-uppercase pb-3">Acacia Blocks</h1>
-                <p className="font_lg pb-9">
-                  Find fun and freedom in transforming your spaces with our
-                  modular furniture series. Simply connect, stack and mount the
-                  blocks to build your own creative storage display.
-                </p>
-                <p>
-                  <Link to="/">
-                    <button
-                      type="button"
-                      className="btn btn-outline btn-outline-large"
-                    >
-                      Discover More
-                    </button>
-                  </Link>
-                </p>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide
-              className={`${indexModule.banner_home} d-flex flex-column justify-content-center align-items-start h-100`}
-              style={{
-                background: "url(/home/banner_2.jpg) center center no-repeat",
-                backgroundSize: "cover",
-              }}
-            >
-              <h1 className="text-uppercase pb-3">M.INT club</h1>
+            <div className="d-flex flex-column justify-content-center align-items-start h-100">
+              <h1 className="text-uppercase pb-3">Acacia Blocks</h1>
               <p>
-                <div className="font_xxl text-uppercase">
-                  <strong>is</strong> <i>more than just </i>
-                  <strong>a rewards program</strong>
-                </div>
-              </p>
-              <p className="font_lg pb-9">
-                Join our community and be a part of something bigger.
+                Find fun and freedom in transforming your spaces with our
+                modular furniture series. Simply connect, stack and mount the
+                blocks to build your own creative storage display.
               </p>
               <p>
                 <Link to="/">
@@ -183,39 +55,62 @@ const HomePage = () => {
                     type="button"
                     className="btn btn-outline btn-outline-large"
                   >
-                    Join now and enjoy 10% off your first purchase
+                    Discover More
                   </button>
                 </Link>
               </p>
-            </SwiperSlide>
-            <SwiperSlide
-              className={`${indexModule.banner_home} d-flex flex-column justify-content-center align-items-start h-100`}
-              style={{
-                background: "url(/home/banner_3.jpg) center center no-repeat",
-                backgroundSize: "cover",
-              }}
-            >
-              <h1 className="text-uppercase pb-3">
-                Free delivery to your door
-              </h1>
-              <p className="font_lg pb-9">
-                Enjoy free delivery when you spend over $150 in a single
-                purchase.
-              </p>
-              <p>
-                <Link to="/">
-                  <button
-                    type="button"
-                    className="btn btn-outline btn-outline-large"
-                  >
-                    Shop Now
-                  </button>
-                </Link>
-              </p>
-            </SwiperSlide>
-          </Swiper>
-        </div>
-        {/* end banner for tablets and larger screens */}
+            </div>
+          </SwiperSlide>
+          <SwiperSlide
+            className={`${indexModule.banner_home} d-flex flex-column justify-content-center align-items-start h-100`}
+            style={{
+              background: "url(/home/banner_2.jpg) center center no-repeat",
+              backgroundSize: "cover",
+            }}
+          >
+            <h1 className="text-uppercase pb-3">M.INT club</h1>
+            <p>
+              <div className="font_xxl text-uppercase">
+                <strong>is</strong> <i>more than just </i>
+                <strong>a rewards program</strong>
+              </div>
+            </p>
+            <p>Join our community and be a part of something bigger.</p>
+            <p>
+              <Link to="/">
+                <button
+                  type="button"
+                  className="btn btn-outline btn-outline-large"
+                >
+                  Join now and enjoy 10% off your first purchase
+                </button>
+              </Link>
+            </p>
+          </SwiperSlide>
+          <SwiperSlide
+            className={`${indexModule.banner_home} d-flex flex-column justify-content-center align-items-start h-100`}
+            style={{
+              background: "url(/home/banner_3.jpg) center center no-repeat",
+              backgroundSize: "cover",
+            }}
+          >
+            <h1 className="text-uppercase pb-3">Free delivery to your door</h1>
+            <p>
+              Enjoy free delivery when you spend over $150 in a single purchase.
+            </p>
+            <p>
+              <Link to="/">
+                <button
+                  type="button"
+                  className="btn btn-outline btn-outline-large"
+                >
+                  Shop Now
+                </button>
+              </Link>
+            </p>
+          </SwiperSlide>
+        </Swiper>
+        {/* end banner */}
         <div id="container_min_modules" className="container">
           <div className="d-none d-md-block">
             <div className="row row_padding">
@@ -300,7 +195,7 @@ const HomePage = () => {
             <div className="col-md-6">
               <h1 className="text-uppercase">Two Sizes.</h1>
               <div className="text_indent">
-                <h2 className="text-uppercase mb-4">Endless Configurations.</h2>
+                <h3 className="text-uppercase mb-4">Endless Configurations.</h3>
                 <p>Stack and arrange the modules in any way you want.</p>
               </div>
             </div>
@@ -317,9 +212,9 @@ const HomePage = () => {
               </video>
             </div>
             <div className="col-md-6 order-1 order-md-2">
-              <h2 className="text-uppercase">Magnetic</h2>
+              <h3 className="text-uppercase">Magnetic</h3>
               <div className="text_indent">
-                <h2 className="text-uppercase mb-4">Connectors.</h2>
+                <h3 className="text-uppercase mb-4">Connectors.</h3>
                 <p>
                   Each module is connected by magnetic connectors which allows
                   flexible arrangements to suit your needs and spaces.
@@ -331,7 +226,7 @@ const HomePage = () => {
             <div className="col-md-6">
               <h1 className="text-uppercase">Flexible</h1>
               <div className="text_indent">
-                <h2 className="text-uppercase mb-4">Storage Compartments.</h2>
+                <h3 className="text-uppercase mb-4">Storage Compartments.</h3>
                 <p>
                   Choose from a selection of doors, drawers and space dividers
                   to satisfy your storage requirements.
@@ -351,7 +246,7 @@ const HomePage = () => {
               </video>
             </div>
             <div className="col-md-6 order-1 order-md-2">
-              <h2 className="text-uppercase">Your Space.</h2>
+              <h3 className="text-uppercase">Your Space.</h3>
               <div className="text_indent">
                 <h1 className="text-uppercase mb-4">Your Style.</h1>
                 <p>
