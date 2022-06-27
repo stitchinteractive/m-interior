af1475fd266bb759fbb08320576d5bce092bc692
stitchinteractive <boan@stitchinteractive.com>
2022-06-27 11:15:50 +0800

    min

--- src/pages/min-modules.jsx
+++ src/pages/min-modules.jsx
@@ -11,6 +11,16 @@ import { ProductListItem } from "../components/product-list-item"
 import { SubBanner } from "../components/subpage-banner"
 import { BackToTop } from "../components/back-to-top"
 
+// import Swiper core and required modules
+import { Navigation, A11y } from "swiper"
+import { Swiper, SwiperSlide } from "swiper/react"
+
+// import Swiper styles
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import "swiper/css/scrollbar"
+
 // step 2: define component
 const Shop = () => {
   gsap.registerPlugin(ScrollTrigger)
@@ -36,7 +46,7 @@ const Shop = () => {
             <NavShop />
           </div>
           <div className="col-lg-9">
-            <div className="row mb-3">
+            <div className="row mb-4">
               <div className="col">
                 <SubBanner
                   background="/shop/min_modules/banner.jpg"
@@ -45,36 +55,36 @@ const Shop = () => {
                 />
               </div>
             </div>
-            <div className="row">
-              <div class="col-12 col-md-6 col-lg-4 mb-3">
+            <div className="row mb-100">
+              <div class="col-12 col-md-6 col-lg-4 mb-4">
                 <ProductListItem
                   image="/shop/min_modules/bedside_table_small_2.png"
                   name="Small Side Table"
                   price="205"
                 />
               </div>
-              <div class="col-12 col-md-6 col-lg-4 mb-3">
+              <div class="col-12 col-md-6 col-lg-4 mb-4">
                 <ProductListItem
                   image="/shop/min_modules/bookshelf_tall_3.png"
                   name="Tall Bookshelf"
                   price="205"
                 />
               </div>
-              <div class="col-12 col-md-6 col-lg-4 mb-3">
+              <div class="col-12 col-md-6 col-lg-4 mb-4">
                 <ProductListItem
                   image="/shop/min_modules/bedroom_chest_small.png"
                   name="Small Bedroom Chest"
                   price="205"
                 />
               </div>
-              <div class="col-12 col-md-6 col-lg-4 mb-3">
+              <div class="col-12 col-md-6 col-lg-4 mb-4">
                 <ProductListItem
                   image="/shop/min_modules/bookshelf_full.png"
                   name="Full Bookshelf"
                   price="205"
                 />
               </div>
-              <div class="col-12 col-lg-8 mb-3">
+              <div class="col-12 col-lg-8 mb-4">
                 <ProductListItem
                   image="/shop/min_modules/tv_console_small_2.png"
                   name="Small Tv Console"
@@ -83,9 +93,80 @@ const Shop = () => {
               </div>
             </div>
             <div className="row">
-              <div className="col">
+              <div className="col-12">
                 <h3 className="text-uppercase py-5">Min+Modules Extras</h3>
-                <BackToTop />
+              </div>
+              <div className="row">
+                <div className="col-12">
+                  <Swiper
+                    className="product_list_swiper d-flex align-items-stretch h-100"
+                    // install Swiper modules
+                    modules={[Navigation, A11y]}
+                    spaceBetween={10}
+                    navigation
+                    breakpoints={{
+                      320: {
+                        slidesPerView: 1,
+                      },
+                      768: {
+                        slidesPerView: 2,
+                      },
+                      1280: {
+                        slidesPerView: 3,
+                      },
+                    }}
+                    onSwiper={(swiper) => console.log(swiper)}
+                    onSlideChange={() => console.log("slide change")}
+                  >
+                    <SwiperSlide className="d-flex flex-column h-100">
+                      <ProductListItem
+                        image="/shop/acacia/acacia_block_1.png"
+                        name="Min+Modules Sample Kit"
+                        price="15"
+                      />
+                    </SwiperSlide>
+                    <SwiperSlide className="d-flex flex-column h-100">
+                      <ProductListItem
+                        image="/shop/acacia/acacia_block_bundle_5.png"
+                        name="Big Module"
+                        price="100"
+                      />
+                    </SwiperSlide>
+                    <SwiperSlide className="d-flex flex-column h-100">
+                      <ProductListItem
+                        image="/shop/min_modules/tv_console_small.png"
+                        name="Door"
+                        price="35"
+                      />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <ProductListItem
+                        image="/shop/min_modules/bookshelf_tall_3.png"
+                        name="Small Module"
+                        price="60"
+                      />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <ProductListItem
+                        image="/shop/acacia_pets/mewwy_go_round.png"
+                        name="Big Module"
+                        price="100"
+                      />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <ProductListItem
+                        image="/shop/acacia_pets/mewwy_go_round.png"
+                        name="Door"
+                        price="35"
+                      />
+                    </SwiperSlide>
+                  </Swiper>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-12">
+                  <BackToTop />
+                </div>
               </div>
             </div>
           </div>
