c3ecb5588fd784c901276810a775ea51248b7da0
stitchinteractive <boan@stitchinteractive.com>
2022-07-10 13:35:50 +0800

    search result

--- src/pages/search-results.jsx
+++ src/pages/search-results.jsx
@@ -31,7 +31,7 @@ const Shop = () => {
       <div className="container">
         <div className="row row_padding">
           <div className="col-12 col-lg-6 offset-lg-3">
-            <div className="input-group my-3">
+            <div className="input-group my-3 d-flex justify-content-between">
               <input
                 type="text"
                 className="form-control-sm txt_search"
@@ -46,7 +46,7 @@ const Shop = () => {
               </button>
             </div>
           </div>
-          <div className="col-12 text-center">
+          <div className="col-12 text-center mt-1">
             Popular Search Terms:
             <Link to="/" className="px-3">
               Acacia Blocks
@@ -67,10 +67,10 @@ const Shop = () => {
               <div className="row mb-100">
                 <div className="col-12">
                   <div className="font_lg text-uppercase font_semibold mb-5">
-                    There are 3 results found.
+                    There are 4 result(s) found.
                   </div>
                 </div>
-                <div className="col-12 col-md-6 col-lg-4 mb-4">
+                <div className="col-12 col-md-6 col-lg-3 mb-4">
                   <ProductListItem
                     url="/shop-details"
                     image="/shop/min_modules/bedside_table_small_2.png"
@@ -123,7 +123,7 @@ const Shop = () => {
                     }
                   />
                 </div>
-                <div className="col-12 col-md-6 col-lg-4 mb-4">
+                <div className="col-12 col-md-6 col-lg-3 mb-4">
                   <ProductListItem
                     url="/shop-details"
                     image="/shop/min_modules/bookshelf_tall_3.png"
@@ -176,7 +176,60 @@ const Shop = () => {
                     }
                   />
                 </div>
-                <div className="col-12 col-md-6 col-lg-4 mb-4">
+                <div className="col-12 col-md-6 col-lg-3 mb-4">
+                  <ProductListItem
+                    url="/shop-details"
+                    image="/shop/min_modules/bedroom_chest_small.png"
+                    name="Small Bedroom Chest"
+                    price="205"
+                    color_1={
+                      <li>
+                        <img src="/icons/color_brown_white.png" alt="" />
+                      </li>
+                    }
+                    color_2={
+                      <li>
+                        <img src="/icons/color_black.png" alt="" />
+                      </li>
+                    }
+                    color_3={
+                      <li>
+                        <img src="/icons/color_dark_brown_white.png" alt="" />
+                      </li>
+                    }
+                    color_4={
+                      <li>
+                        <img src="/icons/color_black_white.png" alt="" />
+                      </li>
+                    }
+                    color_5={
+                      <li>
+                        <img src="/icons/color_white_grey.png" alt="" />
+                      </li>
+                    }
+                    color_6={
+                      <li>
+                        <img src="/icons/color_black_white.png" alt="" />
+                      </li>
+                    }
+                    color_7={
+                      <li>
+                        <img src="/icons/color_black_grey.png" alt="" />
+                      </li>
+                    }
+                    color_8={
+                      <li>
+                        <img src="/icons/color_blue_yellow.png" alt="" />
+                      </li>
+                    }
+                    color_9={
+                      <li>
+                        <img src="/icons/color_brown_black.png" alt="" />
+                      </li>
+                    }
+                  />
+                </div>
+                <div className="col-12 col-md-6 col-lg-3 mb-4">
                   <ProductListItem
                     url="/shop-details"
                     image="/shop/min_modules/bedroom_chest_small.png"
