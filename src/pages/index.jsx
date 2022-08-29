caa9e13e4020ef5cb5654d6bb5304fa7adb500a2
Alex <asjtan@gmail.com>
2022-08-29 15:22:01 +0800

    test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -613,7 +613,7 @@ const HomePage = () => {
               }}
             >
               <h1 className="text-uppercase pb-3">
-                Free delivery to your door
+                Free delivery to your door test
               </h1>
               <p>
                 Enjoy free delivery when you spend over $150 in a single
