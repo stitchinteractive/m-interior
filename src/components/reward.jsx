141b523376be0c5753b465f780522ac80cd00d47
stitchinteractive <boan@stitchinteractive.com>
2022-11-06 15:52:04 +0800

    rebuild

--- src/components/reward.jsx
+++ src/components/reward.jsx
@@ -1,34 +1,39 @@
 // step 1: import
-import React, { useState } from 'react';
-import { Link } from "gatsby"
+import React, { useState } from "react"
+// import { Link } from "gatsby"
 import Button from "react-bootstrap/Button"
 import Modal from "react-bootstrap/Modal"
 
 // step 2: define
 export function Reward(props) {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false)
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
   const handleClick = async () => {
     console.log("handle clicked")
     const options = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        accept: 'application/json',
-        'content-type': 'application/json'
+        accept: "application/json",
+        "content-type": "application/json",
       },
-      body: JSON.stringify({customer_email: props.email, redemption_option_id: props.id})
-    };
-    
-    fetch('https://loyalty.yotpo.com/api/v2/redemptions?guid=jx9X-MCEhx-re9u7YIbChg&api_key=KYoD7NmQ6FaibkwxyAcHGgtt', options)
-      .then(response => response.json())
-      .then(response => console.log(response))
-      .then(response => window.location.reload(false))
-      .catch(err => console.error(err));
+      body: JSON.stringify({
+        customer_email: props.email,
+        redemption_option_id: props.id,
+      }),
+    }
+
+    fetch(
+      "https://loyalty.yotpo.com/api/v2/redemptions?guid=jx9X-MCEhx-re9u7YIbChg&api_key=KYoD7NmQ6FaibkwxyAcHGgtt",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .then((response) => window.location.reload(false))
+      .catch((err) => console.error(err))
   }
-  
 
   return (
     <div
@@ -58,28 +63,25 @@ export function Reward(props) {
         <Modal.Header closeButton>
           <Modal.Title>Redemption</Modal.Title>
         </Modal.Header>
-        {
-          props.customer_points >= props.points ? (
-            <Modal.Body>You are about to redeem {props.points} points for {props.discount}. An email with the redemption code will be sent to you after you confirm your redemption. Press Redeem button now to continue.</Modal.Body>
-          ) : (
-            <Modal.Body>Sorry. You do not have enough points to do this redemption.</Modal.Body>
-          )
-        }
-       
+        {props.customer_points >= props.points ? (
+          <Modal.Body>
+            You are about to redeem {props.points} points for {props.discount}.
+            An email with the redemption code will be sent to you after you
+            confirm your redemption. Press Redeem button now to continue.
+          </Modal.Body>
+        ) : (
+          <Modal.Body>
+            Sorry. You do not have enough points to do this redemption.
+          </Modal.Body>
+        )}
+
         <Modal.Footer>
-          <Button onClick={handleClose}>
-            Close
-          </Button>
-          {
-          props.customer_points >= props.points ? (
-            <Button onClick={handleClick}>
-              Redeem
-            </Button>
+          <Button onClick={handleClose}>Close</Button>
+          {props.customer_points >= props.points ? (
+            <Button onClick={handleClick}>Redeem</Button>
           ) : (
             <div></div>
-          )
-        }
-          
+          )}
         </Modal.Footer>
       </Modal>
     </div>
