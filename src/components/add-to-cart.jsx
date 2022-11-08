c6e9a924f1b9ec04017eee6645b5789e15b20933
stitchinteractive <boan@stitchinteractive.com>
2022-11-08 11:11:30 +0800

    add to cart

--- src/components/add-to-cart.jsx
+++ src/components/add-to-cart.jsx
@@ -8,15 +8,18 @@ import Modal from "react-bootstrap/Modal"
 
 export function AddToCart({ variantId, quantity, available, notes, ...props }) {
   const { addVariantToCart, checkout, loading } = React.useContext(StoreContext)
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false)
   const handleClose = () => {
     setShow(false)
     // navigate(`/earn-points`)
-  };
-  const handleShow = () => setShow(true);
+  }
+  const handleShow = () => setShow(true)
   const emptyCart = checkout.lineItems.length === 0
   const handleCheckout = () => {
-    var checkoutUrl = checkout.webUrl.replace(process.env.GATSBY_SHOPIFY_STORE_URL, process.env.GATSBY_DOMAIN);
+    var checkoutUrl = checkout.webUrl.replace(
+      process.env.GATSBY_SHOPIFY_STORE_URL,
+      process.env.GATSBY_DOMAIN
+    )
     window.location.assign(checkoutUrl)
   }
   const handleViewCart = () => {
@@ -34,30 +37,40 @@ export function AddToCart({ variantId, quantity, available, notes, ...props }) {
     <div>
       <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
-          <Modal.Title>Cart</Modal.Title>
+          <Modal.Title>Item added to cart</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           {!emptyCart ? (
             <div>
-              <Button variant="secondary" onClick={handleViewCart}>
-                View my cart ({checkout.lineItems.length})
-              </Button>
-              <br /><br />
-              <Button variant="secondary" onClick={handleCheckout}>
-                Check out
-              </Button>
+              <p className="pb-3">
+                <Button
+                  className="btn btn-outline btn-black w-100"
+                  onClick={handleViewCart}
+                >
+                  View my cart ({checkout.lineItems.length})
+                </Button>
+              </p>
+              <p className="pb-0">
+                <Button
+                  className="btn btn-tertiary w-100"
+                  onClick={handleCheckout}
+                >
+                  Check out
+                </Button>
+              </p>
             </div>
           ) : (
-            <div>
-              
-            </div>
+            <div></div>
           )}
-          
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Continue shopping
-          </Button>
+          <a
+            href="javascript:void(0);"
+            onClick={handleClose}
+            className="font_sm link_underline"
+          >
+            Continue shopping &gt;
+          </a>
         </Modal.Footer>
       </Modal>
       <button
