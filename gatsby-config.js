8903d5a124f53a90ca891b195ec3e8b28dc0fa05
Alex Tan <asjtan@gmail.com@users.noreply.github.com>
2023-01-24 20:08:09 +0800

    setup contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,14 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `oct3hb8a5g86`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
