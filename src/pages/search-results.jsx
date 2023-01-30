6b6d315b7a32c4fb77c574fdc1e6df5d75f71e63
Alex Tan <asjtan@gmail.com@users.noreply.github.com>
2023-01-31 01:29:17 +0800

    updated search

--- src/pages/search-results.jsx
+++ src/pages/search-results.jsx
@@ -7,10 +7,79 @@ import { Layout } from "../components/layout"
 import { Membership } from "../components/membership"
 import { Testimonials } from "../components/testimonials"
 import { ProductListItem } from "../components/product-list-item"
+import CrossIcon from "../icons/cross"
+import SortIcon from "../icons/sort"
+import FilterIcon from "../icons/filter"
 import SearchIcon from "../icons/search"
+import { graphql } from "gatsby"
+import { useProductSearch } from "../utils/hooks"
+import { getValuesFromQuery } from "../utils/search"
+import { getCurrencySymbol } from "../utils/format-price"
+import debounce from "debounce"
+import { CgChevronRight, CgChevronLeft } from "react-icons/cg"
+import { Spinner } from "../components/progress"
+import { Filters } from "../components/filters"
+import { SearchProvider } from "../context/search-provider"
+import {
+  visuallyHidden,
+  main,
+  search,
+  searchIcon,
+  sortSelector,
+  results,
+  productList as productListStyle,
+  productListItem,
+  pagination,
+  paginationButton,
+  progressStyle,
+  resultsStyle,
+  filterStyle,
+  clearSearch,
+  searchForm,
+  sortIcon,
+  filterButton,
+  filterTitle,
+  modalOpen,
+  activeFilters,
+  filterWrap,
+  emptyState,
+} from "./search-page.module.css"
+
+const DEFAULT_PRODUCTS_PER_PAGE = 24
+
+export async function getServerData({ query, ...rest }) {
+  const { getSearchResults } = require("../utils/search")
+  const products = await getSearchResults({
+    query,
+    count: DEFAULT_PRODUCTS_PER_PAGE,
+  })
+
+  return {
+    props: {
+      query,
+      products,
+    },
+  }
+}
+
+export const query = graphql`
+  query {
+    meta: allShopifyProduct {
+      productTypes: distinct(field: productType)
+      tags: distinct(field: tags)
+      vendors: distinct(field: vendor)
+    }
+  }
+`
 
 // step 2: define component
-const Shop = () => {
+const Shop = ({
+  serverData,
+  data: {
+    meta: { productTypes, vendors, tags },
+  },
+  location,
+}) => {
   gsap.registerPlugin(ScrollTrigger)
 
   useLayoutEffect(() => {
@@ -26,24 +95,81 @@ const Shop = () => {
     })
   })
 
+  // These default values come from the page query string
+  const queryParams = getValuesFromQuery(location.search || serverData.query)
+
+  const [filters, setFilters] = React.useState(queryParams)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const initialFilters = React.useMemo(() => queryParams, [])
+  const [sortKey, setSortKey] = React.useState(queryParams.sortKey)
+  // We clear the hash when searching, we want to make sure the next page will be fetched due the #more hash.
+  const shouldLoadNextPage = React.useRef(false)
+
+  const {
+    products,
+    isFetching,
+    filterCount,
+    hasNextPage,
+    hasPreviousPage,
+    fetchNextPage,
+    fetchPreviousPage,
+  } = useProductSearch(
+    filters,
+    {
+      allProductTypes: productTypes,
+      allVendors: vendors,
+      allTags: tags,
+    },
+    sortKey,
+    false,
+    DEFAULT_PRODUCTS_PER_PAGE,
+    serverData.products,
+    initialFilters
+  )
+
+  // Automatically load the next page if "#more" is in the URL
+  React.useEffect(() => {
+    if (location.hash === "#more") {
+      // save state so we can fetch it when the first page got fetched to retrieve the cursor
+      shouldLoadNextPage.current = true
+    }
+
+    if (shouldLoadNextPage.current) {
+      if (hasNextPage) {
+        fetchNextPage()
+      }
+
+      shouldLoadNextPage.current = false
+    }
+  }, [location.hash, hasNextPage, fetchNextPage])
+
+  const currencyCode = getCurrencySymbol(
+    serverData.products?.[0]?.node?.priceRangeV2?.minVariantPrice?.currencyCode
+  )
+
+  // debugger
+  // console.log(serverData.products)
+
   return (
     <Layout>
       <div className="container">
         <div className="row row_padding">
           <div className="col-12 col-lg-6 offset-lg-3">
             <div className="input-group my-3 d-flex justify-content-between">
-              <input
+              <SearchBar defaultTerm={filters.term} setFilters={setFilters} />
+              {/* <input
                 type="text"
                 className="form-control-sm txt_search"
                 placeholder="Search"
                 aria-label="Search"
                 aria-describedby="search"
               />
+
               <button className="btn btn_search" type="button">
                 <Link to="/search-results">
                   <SearchIcon />
                 </Link>
-              </button>
+              </button> */}
             </div>
           </div>
           <div className="col-12 text-center mt-1">
@@ -65,12 +191,39 @@ const Shop = () => {
           <div className="row row_padding">
             <div className="col-12">
               <div className="row mb-100">
+              {!isFetching && (
                 <div className="col-12">
+                  <div className="font_lg text-uppercase font_semibold mb-5">
+                    There are {products.length} result(s) found.
+                  </div>
+                </div>
+                
+              )}
+                {/* <div className="col-12">
                   <div className="font_lg text-uppercase font_semibold mb-5">
                     There are 4 result(s) found.
                   </div>
+                </div> */}
+                {!isFetching && products.map(({ node }, index) => (
+                  
+                  <div className="col-12 col-md-6 col-lg-3 mb-4">
+                  <ProductListItem
+                    url={"/shop/detail/" + node.handle}
+                    image={node.images.edges[0]?.node.originalSrc}
+                    name={node.title}
+                    price={node.priceRangeV2.minVariantPrice.amount}
+                  />
                 </div>
-                <div className="col-12 col-md-6 col-lg-3 mb-4">
+                ))}
+                {hasPreviousPage || hasNextPage ? (
+                  <Pagination
+                    previousPage={fetchPreviousPage}
+                    hasPreviousPage={hasPreviousPage}
+                    nextPage={fetchNextPage}
+                    hasNextPage={hasNextPage}
+                  />
+                ) : undefined}
+                {/* <div className="col-12 col-md-6 col-lg-3 mb-4">
                   <ProductListItem
                     url="/shop-details"
                     image="/shop/min_modules/bedside_table_small_2.png"
@@ -281,7 +434,7 @@ const Shop = () => {
                       </li>
                     }
                   />
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
@@ -297,5 +450,79 @@ const Shop = () => {
   )
 }
 
+function SearchBar({ defaultTerm, setFilters }) {
+  const [term, setTerm] = React.useState(defaultTerm)
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedSetFilters = React.useCallback(
+    debounce((value) => {
+      setFilters((filters) => ({ ...filters, term: value }))
+    }, 200),
+    [setFilters]
+  )
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()} className={searchForm}>
+      <SearchIcon aria-hidden className={searchIcon} />
+      <input
+        type="text"
+        value={term}
+        onChange={(e) => {
+          setTerm(e.target.value)
+          debouncedSetFilters(e.target.value)
+        }}
+        placeholder="Search..."
+      />
+      {term ? (
+        <button
+          className={clearSearch}
+          type="reset"
+          onClick={() => {
+            setTerm("")
+            setFilters((filters) => ({ ...filters, term: "" }))
+          }}
+          aria-label="Clear search query"
+        >
+          <CrossIcon />
+        </button>
+      ) : undefined}
+    </form>
+  )
+}
+/**
+ * Shopify only supports next & previous navigation
+ */
+function Pagination({ previousPage, hasPreviousPage, nextPage, hasNextPage }) {
+  return (
+    <nav className={pagination}>
+      <button
+        className={paginationButton}
+        disabled={!hasPreviousPage}
+        onClick={previousPage}
+        aria-label="Previous page"
+      >
+        <CgChevronLeft />
+      </button>
+      <button
+        className={paginationButton}
+        disabled={!hasNextPage}
+        onClick={nextPage}
+        aria-label="Next page"
+      >
+        <CgChevronRight />
+      </button>
+    </nav>
+  )
+}
+
 // step 3: export
-export default Shop
+
+export default function SearchPageTemplate(props) {
+  return (
+    <SearchProvider>
+      <Shop {...props} />
+    </SearchProvider>
+  )
+}
+
+//export default Shop
