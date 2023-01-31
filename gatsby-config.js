523548c0b08dfb8f0b5cb87b4c5a0096942c1aac
Alex Tan <asjtan@gmail.com@users.noreply.github.com>
2023-02-01 01:46:24 +0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    "gatsby-plugin-react-helmet",
+    //"gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-gatsby-cloud",
     {
